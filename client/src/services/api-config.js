7fa08476772d7d6ab50d2d2106f996b6cf499208
willwardlow <wardlow.william@gmail.com>
2021-04-05 10:25:39 -0500

    Created deployed link for backend

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'deployed link goes here' : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://pizza-party-api.herokuapp.com/' : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: baseUrl

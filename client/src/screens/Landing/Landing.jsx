c0d9f7cb6b208da3e25042538eccd37c6b2dd94b
willwardlow <wardlow.william@gmail.com>
2021-04-22 17:38:03 -0500

    finished landing page

--- client/src/screens/Landing/Landing.jsx
+++ client/src/screens/Landing/Landing.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import ImageCarousel from '../../components/ImageCarousel/ImageCarousel'
+import ImageCarousel from '../../components/ImageCarousel/ImageCarousel';
+import './Landing.css'
 
 export default function Landing() {
   return (
-    <div className='landing-container'> 
-      <ImageCarousel />
+    <div className='landing-container'>
+       <div className='landing-text'>
+        <h3>A place for Chicagoans to share where to find the best pizza in Chicago. Login / Register to join the party!</h3> 
+      </div>
+      <div className='carousel-container'>
+        <ImageCarousel />
+      </div>
+     
     </div>
   )
 }

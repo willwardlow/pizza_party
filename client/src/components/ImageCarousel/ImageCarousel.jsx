6e9d3ee75319b45f1d3d6aa24a446b69d31c5f10
willwardlow <wardlow.william@gmail.com>
2021-04-22 17:03:30 -0500

    added carousel to landing page

--- client/src/components/ImageCarousel/ImageCarousel.jsx
+++ client/src/components/ImageCarousel/ImageCarousel.jsx
@@ -12,7 +12,7 @@ export default function ImageCarousel() {
         showStatus={false}
         infiniteLoop
         interval='5000'
-        transitionTime='1000'
+        transitionTime='2000'
       className='images'>
       <img width='80%' src='https://images.unsplash.com/photo-1494522855154-9297ac14b55f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80' alt='millenium-park'/>
       <img width='80%' src='https://images.unsplash.com/photo-1494522358652-f30e61a60313?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80' alt='chicago-theater'/>
